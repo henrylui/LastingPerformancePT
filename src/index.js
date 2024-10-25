710c9dba9acef68b9c47ec7bf8459b9278833fee
henryluimcit <henrylui@seas.upenn.edu>
2024-10-25 15:07:24 -0700

    Fix deploment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,17 @@
 // src/index.js
 import React from 'react';
-import { createRoot, hydrateRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 import './index.css';
 
 const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
 
-if (rootElement.hasChildNodes()) {
-  hydrateRoot(rootElement, <App />);
-} else {
-  createRoot(rootElement).render(<App />);
-}
+// Wrap rendering in a setTimeout to ensure DOM is fully loaded
+setTimeout(() => {
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}, 0);
