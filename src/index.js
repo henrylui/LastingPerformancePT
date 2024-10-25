e132db65a3150d1ea3820616a1af8aa615efe7d4
henryluimcit <henrylui@seas.upenn.edu>
2024-10-25 14:22:32 -0700

    Add more SEO features

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
 // src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import './index.css';
+import { createRoot, hydrateRoot } from 'react-dom/client';
 import App from './App';
+import './index.css';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
-);
+const rootElement = document.getElementById('root');
+
+if (rootElement.hasChildNodes()) {
+  hydrateRoot(rootElement, <App />);
+} else {
+  createRoot(rootElement).render(<App />);
+}
